8a50f6d3a2926dfab4eab3166266f62d98559554
tfeydev <tfeydeveloper@gmail.com>
2025-11-28 22:40:16 -0300

    feat: add initial project structure and create module 01-books

---
+++ 01-books/src/main/java/dev/tfey/books/BooksApplication.java
@@ -0,0 +1,13 @@
+package dev.tfey.books;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(BooksApplication.class, args);
+	}
+
+}
