8881a97ed6bcf1c701211cb179eee15994946586
tfeydev <tfeydeveloper@gmail.com>
2025-12-02 20:42:05 -0300

    feat: update books.continued

---
+++ 02-books-continued/src/main/java/dev/tfey/books/continued/BooksApplication.java
@@ -0,0 +1,13 @@
+package dev.tfey.books.continued;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(BooksApplication.class, args);
+	}
+
+}
